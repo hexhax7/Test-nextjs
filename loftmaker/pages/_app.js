8f9b3bf3b310b7b6644468857850b7855576aa5c
hexhax7 <87332576+hexhax7@users.noreply.github.com>
2023-09-13 17:31:53 +0100

    Analytics

--- loftmaker/pages/_app.js
+++ loftmaker/pages/_app.js
@@ -1,19 +1,21 @@
 import '@/styles/globals.css'
 import { Roboto } from 'next/font/google'
-import { useRouter } from 'next/router';
 
+
+
+import { Analytics } from '@vercel/analytics/react';
 
 const roboto = Roboto({
   subsets: ["latin"],
   weight: "400",
 });
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps } ) {
   
   return (
     <main className={roboto.className}>
       <Component {...pageProps} />
-      
+      <Analytics />
     </main>
 
     
